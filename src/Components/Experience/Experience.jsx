c5192bce38bce76fb9ae5102140652315c80065e
Harnoor Sagar <harnoor.sagar@gmail.com>
2024-09-01 22:22:28 -0400

    fixed link errors

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -35,7 +35,7 @@ const Experience = () => {
                         As a Team Leader in the Youth In Policing, I supervised 80 students, worked with the snior leadership team to ensure smooth running of daily activities, and and led my team of 10 students to ensure the success of our event, the IMPACT Youth Forum.
                     </p>
                     <div className="links">
-                        <a href='www.linkedin.com/in/harnoor-sagar' target='_blank'>
+                        <a href='https://www.linkedin.com/in/harnoor-sagar/' target='_blank'>
                             <button className="experience1-learnmore">
                                 <p>Learn More!</p>
                             </button>
@@ -68,7 +68,7 @@ const Experience = () => {
                         As a student in the Youth In Policing, I collaborated and networked with several like-minded colleagues to ensure the success of Youth In Policing events. Specifically, I planned and worked with team members to plan the Para Sports events, and assisted 40+ players with disabilities over the course of the events.
                     </p>
                     <div className="links">
-                        <a href='www.linkedin.com/in/harnoor-sagar' target='_blank'>
+                        <a href='https://www.linkedin.com/in/harnoor-sagar/'target='_blank'>
                             <button className="experience2-learnmore">
                                 <p>Learn More!</p>
                             </button>
@@ -87,7 +87,7 @@ const Experience = () => {
                         As a Volunteer with the Durham College, I created social media content that would promote events that the School of IT, Business, and Management were conducting. I interacted with professors to gather information needed to create the content for various events such as hackathons, workshops, project shocases, and more!
                     </p>
                     <div className="links">
-                        <a href='www.linkedin.com/in/harnoor-sagar' target='_blank'>
+                        <a href='https://www.linkedin.com/in/harnoor-sagar/' target='_blank'>
                             <button className="experience3-learnmore">
                                 <p>Learn More!</p>
                             </button>
